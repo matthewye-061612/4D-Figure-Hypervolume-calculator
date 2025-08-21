7018ae8306c599744c77bae0fd3edcb043121fd2
matthewye-061612 <matthewye.tp@gmail.com>
2025-08-20 19:47:51 -0700

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    //Edit the values only unless you know what you're doing and can responsibly add to, modify, or delete parts of the program to suit your needs.
+    double length = 4;
+    double width = 4;
+    double height = 4;
+    double fourthVariable = 4;
+
+    double hypervolume = length * width * height * fourthVariable;
+
+    std::cout << "The hypervolume of the 4D figure is: " << hypervolume << std::endl;
+
+    return 0;
+}
